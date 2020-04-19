14ebd68cb44dfa819e447990744ffc643ac9851e
13020180018 <62000318+13020180018@users.noreply.github.com>
2020-04-19 19:02:02 +0700

    Add files via upload

---
+++ Tugas 7 PBO/Vehicle.java
@@ -0,0 +1,12 @@
+public abstract class Vehicle {
+	//method function yang akan menampilkan keterangana alat transportasi
+	public void function(){
+	   System.out.println("Transportasi Tools");
+	}
+	//method ruel menampilkan keterangan ruel (bahan bakar);
+	public void ruel(){
+	    System.out.println("fuels");
+	}
+	//method abstrak walk yang bertipe void
+	public abstrack walk();
+}
